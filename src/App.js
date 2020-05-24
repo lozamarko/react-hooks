b5d8434a21c32487475e855bba191ac552ed5872
Marko Lozančić <lozamarko@hotmail.com>
2020-05-24 14:04:55 +0200

    pull request test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "./useForm";
 
 const App = () => {
   const [values, handleChange] = useForm({ email: "", password: "" });
-  const [values2, handleChange2] = useForm({ firstName: "", lastName: "" });
+  // const [values2, handleChange2] = useForm({ firstName: "", lastName: "" });
+  
+  useEffect(()=> {
+    
+  }, [values.password])
 
   return (
     <div>
