1504ca26e04a32aa5a64f10452715f5ccae052eb
Marko Lozančić <lozamarko@hotmail.com>
2020-05-24 14:14:13 +0200

    formatted the code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import { useForm } from "./useForm";
 
 const App = () => {
   const [values, handleChange] = useForm({ email: "", password: "" });
-  // const [values2, handleChange2] = useForm({ firstName: "", lastName: "" });
-  
-  useEffect(()=> {
-    
-  }, [values.password])
+
+  useEffect(() => {}, [values.password]);
 
   return (
     <div>
